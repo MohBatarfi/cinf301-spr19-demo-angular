c864329869f693718597e9bcf27e3d217bc84c1f
isabel <iholi@stetson.edu>
2019-04-26 05:10:57 -0400

    added code about two-way data binding

---
+++ angular-demo/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent
+{
+  algorithmType = "Quick Sort"
+  changeAlgorithm(ev)
+  {
+    this.algorithmType = ev.target.value;
+  }
+
+  rankingNumber = "9"
+  changeRanking(ev)
+  {
+    this.rankingNumber = ev.target.value;
+  }
+
+  image = "https://i.makeagif.com/media/11-29-2015/Uf4BMe.gif"
+  changeImage(ev)
+  {
+    this.image = ev.target.value;
+  }
+}
