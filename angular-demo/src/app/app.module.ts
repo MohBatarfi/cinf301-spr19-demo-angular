48b50f2d5dd7695596ca984dd41f75b57b005742
isabel <iholi@stetson.edu>
2019-04-26 05:19:11 -0400

    added code about routing

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { RouterModule, Routes} from '@angular/router';
+import { AlgorithmsComponent } from './algorithms/algorithms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlgorithmsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+  {path: 'algorithmSort', component: AlgorithmsComponent}
+])
   ],
   providers: [],
   bootstrap: [AppComponent]
